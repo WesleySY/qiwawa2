aa5786310c1a13aa5187a364fbbc0e4d08a234d1
Wesley Yian <ysyiau@gmail.com>
2023-07-13 17:47:13 +0800

    Update location.php

--- sycw/location.php
+++ sycw/location.php
@@ -4,13 +4,16 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to c
 Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
 -->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+
+<body>
+    <?php
+    echo "Hello Worrld";
+    ?>
+</body>
+
 </html>
