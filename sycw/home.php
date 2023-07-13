32c67adb8c6e6a22886320e22d573f0cb4cd3140
qiwawa1 <tchiwah591@gmail.com>
2023-07-13 16:46:49 +0800

    asd

--- sycw/home.php
+++ sycw/home.php
@@ -9,26 +9,22 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
         <title></title>
         <style>
             ul{
-                background-color: grey;
                 text-align:left;
             }
             .li1{
-                text-align: center;
-                color :aliceblue;
-                
                 display:inline;
             }
         </style>
     </head>
     <body>
         <ul>
-            <li class="li1"><a href="home.php"><img src ="companylogo.png" ></a></li>
+            <li class="li1"><a href="home.php"><img src ="../companylogo.png" ></a></li>
             <li class="li1"><a href="movie.php">Movies</a></li>
             <li class="li1"><a href="location.php">Location</a></li>
             <li class="li1"><a href="login.php">Sign in</a></li>
             
         </ul>
-            
+
 
        
     </body>
